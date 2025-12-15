010f1fc59ae10a771927f1238db53cc961d56edf
Zokky2e <icro4.gaming@hotmail.com>
2025-12-15 20:27:06 +0100

    simple start up hint

--- app/main.py
+++ app/main.py
@@ -5,7 +5,8 @@ app = FastAPI()
 
 app.include_router(auth_routes.router, prefix="/auth", tags=["Auth"])
 app.include_router(money_routes.router, prefix="/money", tags=["Auth"])
-
+#start server with:
+#uvicorn app.main:app --reload --host 0.0.0.0 --port 5000
 @app.get("/")
 def root():
     return {"message": "Hello from FastAPI!"}
